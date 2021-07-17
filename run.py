113b425543c2c02a07809ad243787767737b8194
mosull20 <mosullivan@mac.com>
2021-07-17 09:57:16 +0000

    Add function to return number of sandwiches to make for next market

--- run.py
+++ run.py
@@ -133,6 +133,16 @@ def calculate_stock_data(data):
     return new_stock_data
 
 
+def get_stock_values(data):
+    """
+    """
+    print("Make the following number of sandwiches for next market")
+    headings = SHEET.worksheet("sales").get_all_values()[0]
+    values = stock_data
+    sandwich_dictionary = dict(zip(headings, values))
+    print(sandwich_dictionary)
+
+
 def main():
     """
     Run all program functions
@@ -145,7 +155,9 @@ def main():
     sales_columns = get_last_5_entries_sales()
     stock_data = calculate_stock_data(sales_columns)
     update_worksheet(stock_data, "stock")
+    return stock_data
 
 
 print("Welcome to Love Sandwiches Data Automation")
-main()
+stock_data = main()
+get_stock_values(stock_data)
